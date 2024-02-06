5ef19fd7ac81a24a013785dbd635f01b4f951b29
Mostafa-Usama <mostafa.usama286@gmail.com>
2024-02-06 22:42:55 +0200

    added animation on scroll, better performance

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 let menuBtn = document.querySelector(".menu-btn")
 let menu = document.querySelector(".menu")
-
+let scrollBtn = document.querySelector(".scroll-btn")
+let main = document.querySelector("main")
+let sections = document.querySelectorAll("section")
 
 menuBtn.addEventListener("click", () => {
     menu.classList.toggle("opened")
     menuBtn.classList.toggle("clicked")
 })
+
+window.addEventListener("scroll", () => {
+    this.scrollY > 300 ? scrollBtn.classList.add("visible") : scrollBtn.classList.remove("visible")
+    
+    sections.forEach(section => {
+        if (this.scrollY + this.innerHeight > section.offsetTop + 150) {
+            section.style.opacity = "1"
+        }
+    })
+        
+})
+
+window.onload = ()=>{
+    main.style.opacity = "1"
+}
